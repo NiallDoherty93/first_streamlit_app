8d4660e76d817237501045e06007bc4f0fa9e2b9
NiallDoherty93 <85573900+NiallDoherty93@users.noreply.github.com>
2024-02-13 19:05:17 +0000

    Update streamlit_app.py 1.3

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 
 streamlit.header('Breakfast Menu')
-streamit.text('egg')
-streamit.text('keal')
-streamit.text('oatmeal')
+streamlit.text('egg')
+streamlit.text('keal')
+streamlit.text('oatmeal')
