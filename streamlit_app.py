c12b0f7c1f59c39da2e0e9572bf6bed77c8e7724
NiallDoherty93 <85573900+NiallDoherty93@users.noreply.github.com>
2024-02-15 20:12:47 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ streamlit.dataframe(fruits_to_show)
          #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
          #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
          #streamlit.dataframe(fruityvice_normalized)
-          streamlit.dataframe()
+         #streamlit.dataframe()
 
 #except URLError as e:
     #streamlit.error()
