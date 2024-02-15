c93d6d6edf4bc810b1d507cd2bcbcb498014dc16
NiallDoherty93 <85573900+NiallDoherty93@users.noreply.github.com>
2024-02-15 19:38:15 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ streamlit.header("The fuit load list contains")
 streamlit.dataframe(my_data_rows)
 
 streamlit.header("What fuit would you like to add?")
-new_fruit = streamlit.text_input('What fruit would you like to add?' + new_fruit)
+new_fruit = streamlit.text_input('What fruit would you like to add?' ,'')
 streamlit.write('Thanks for adding ', new_fruit)
 
 # Add the new fruit to the list directly (no need for if statement)
